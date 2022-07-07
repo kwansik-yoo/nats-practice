d70e8d7661fa4621d133486da1b20dc78a9da05c
ksyoo <nextree8@partner.samsung.com>
2022-07-07 11:35:44 +0900

    Initial commit

---
+++ src/main/java/com/example/natspractice/NatspracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.natspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatspracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatspracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/natspractice/NatspracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.natspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatspracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
